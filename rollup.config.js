43d598ea6088cd5cad62ffb70b5ce186d40ddd76
savucosmin9183 <savucosmin9183@gmail.com>
2024-11-21 13:22:05 +0200

    updated build settings

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import merge from 'deepmerge';
-// use createSpaConfig for bundling a Single Page App
+import copy from 'rollup-plugin-copy'; // Import the copy plugin
 import { createSpaConfig } from '@open-wc/building-rollup';
 
-// use createBasicConfig to do regular JS to JS bundling
-// import { createBasicConfig } from '@open-wc/building-rollup';
-
+// Base SPA config
 const baseConfig = createSpaConfig({
-  // use the outputdir option to modify where files are output
-  // outputDir: 'dist',
-
-  // if you need to support older browsers, such as IE11, set the legacyBuild
-  // option to generate an additional build just for this browser
-  // legacyBuild: true,
-
-  // development mode creates a non-minified build for debugging or development
   developmentMode: process.env.ROLLUP_WATCH === 'true',
-
-  // set to true to inject the service worker registration into your index.html
   injectServiceWorker: false,
 });
 
 export default merge(baseConfig, {
-  // if you use createSpaConfig, you can use your index.html as entrypoint,
-  // any <script type="module"> inside will be bundled by rollup
   input: './index.html',
 
-  // alternatively, you can use your JS as entrypoint for rollup and
-  // optionally set a HTML template manually
-  // input: './app.js',
+  plugins: [
+    // Add the copy plugin to copy assets from src/assets to dist/assets
+    copy({
+      targets: [
+        { src: 'src/assets/*', dest: 'dist/assets' }, // This copies all files in src/assets to dist/assets
+      ],
+    }),
+  ],
 });
