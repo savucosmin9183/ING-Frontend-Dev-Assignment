e5b144b9b7ea7e6532634b50379db712f395067f
savucosmin9183 <savucosmin9183@gmail.com>
2024-11-21 13:39:49 +0200

    updated css for footer

--- src/MainFooter/main-footer.js
+++ src/MainFooter/main-footer.js
@@ -15,107 +15,110 @@ class MainFooter extends LitElement {
     }
 
     footer a {
-        color: #333333;
-        text-decoration: none;
+      color: #333333;
+      text-decoration: none;
     }
 
-    .mobile-badges{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+    .mobile-badges {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
 
     .mobile-badges .ios .badge {
-        background-image: url(https://www.homebank.ro/hb/hb/assets/images/c0cda0b72a6facf6c999.svg);
-        background-size: contain;
-        background-repeat: no-repeat;
-        width: 120px;
-        height: 40px;
-        margin-bottom: 20px;
+      background-image: url(https://www.homebank.ro/hb/hb/assets/images/c0cda0b72a6facf6c999.svg);
+      background-size: contain;
+      background-repeat: no-repeat;
+      width: 120px;
+      height: 40px;
+      margin-bottom: 20px;
     }
 
     .mobile-badges .android .badge {
-        background-image: url(https://www.homebank.ro/hb/hb/assets/images/6fd448b800ea5db22e3b.svg);
-        background-size: contain;
-        background-repeat: no-repeat;
-        width: 120px;
-        height: 40px;
+      background-image: url(https://www.homebank.ro/hb/hb/assets/images/6fd448b800ea5db22e3b.svg);
+      background-size: contain;
+      background-repeat: no-repeat;
+      width: 120px;
+      height: 40px;
     }
 
-    .desktop-badges{
-        display: none;
-        width: 100%;
-        justify-content: center;
+    .desktop-badges {
+      display: none;
+      width: 100%;
+      justify-content: center;
+    }
+
+    .ios {
+      margin-right: 150px;
     }
 
     .desktop-badges .ios .badge {
-        background-image: url(https://www.homebank.ro/hb/hb/assets/images/1bc9e6e4a1e2a2b635c9.svg);
-        width: 116px;
-        height: 168px;
-        background-size: 100%;
-        margin-right: 150px;
+      background-image: url(https://www.homebank.ro/hb/hb/assets/images/1bc9e6e4a1e2a2b635c9.svg);
+      width: 116px;
+      height: 168px;
+      background-size: 100%;
     }
 
     .desktop-badges .android .badge {
-        background-image: url(https://www.homebank.ro/hb/hb/assets/images/ad8d99aca7ffd7008b58.svg);
-        width: 116px;
-        height: 168px;
-        background-size: 100%;
+      background-image: url(https://www.homebank.ro/hb/hb/assets/images/ad8d99aca7ffd7008b58.svg);
+      width: 116px;
+      height: 168px;
+      background-size: 100%;
     }
 
-    .help-items{
-        margin-top: 30px;
-        margin-bottom: 80px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+    .help-items {
+      margin-top: 30px;
+      margin-bottom: 80px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
 
-    .help-items .item{
-        margin-bottom: 15px;
+    .help-items .item {
+      margin-bottom: 15px;
     }
 
-    .copyright{
-        font-size: 12px;
-        margin-bottom: 40px;
+    .copyright {
+      font-size: 12px;
+      margin-bottom: 40px;
     }
 
     @media (min-width: 576px) {
-        .mobile-badges {
-            display: none;
-        }
+      .mobile-badges {
+        display: none;
+      }
 
-        .desktop-badges{
-            display: flex;
-        }
+      .desktop-badges {
+        display: flex;
+      }
 
-        .help-items{
-            flex-direction: row;
-            justify-content: center;
-            column-gap: 20px;
-        }
+      .help-items {
+        flex-direction: row;
+        justify-content: center;
+        column-gap: 20px;
+      }
 
-        .help-items > * {
-            position: relative;
-        }
+      .help-items > * {
+        position: relative;
+      }
 
-        .help-items > *::after {
-            content: '';
-            position: absolute;
-            top: 0;
-            right: -10px;
-            height: 100%;
-            width: 1px;
-            background-color: #ccc;
-        }
+      .help-items > *::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: -10px;
+        height: 100%;
+        width: 1px;
+        background-color: #ccc;
+      }
 
-        .help-items > *:last-child::after {
-            display: none;
-        }
+      .help-items > *:last-child::after {
+        display: none;
+      }
 
-        .help-items .item{
-            margin-bottom: 0px;
-        }
+      .help-items .item {
+        margin-bottom: 0px;
+      }
     }
   `;
 
@@ -123,43 +126,75 @@ class MainFooter extends LitElement {
     return html`
       <footer>
         <div class="mobile-badges">
-            <div class="ios">
-                <a target="_blank" href="https://itunes.apple.com/ro/app/ing-homebank/id458609168?mt=8&ls=1">
-                    <div class="badge"></div>
-                </a>
-            </div>
-            <div class="android">
-                <a target="_blank" href="https://play.google.com/store/apps/details?id=ro.ing.mobile.banking.android.activity&pli=1">
-                    <div class="badge"></div>
-                </a>
-            </div>
+          <div class="ios">
+            <a
+              target="_blank"
+              href="https://itunes.apple.com/ro/app/ing-homebank/id458609168?mt=8&ls=1"
+            >
+              <div class="badge"></div>
+            </a>
+          </div>
+          <div class="android">
+            <a
+              target="_blank"
+              href="https://play.google.com/store/apps/details?id=ro.ing.mobile.banking.android.activity&pli=1"
+            >
+              <div class="badge"></div>
+            </a>
+          </div>
         </div>
         <div class="desktop-badges">
-            <div class="ios">
-                <a target="_blank" href="https://itunes.apple.com/ro/app/ing-homebank/id458609168?mt=8&ls=1">
-                    <div class="badge"></div>
-                </a>
-            </div>
-            <div class="android">
-                <a target="_blank" href="https://play.google.com/store/apps/details?id=ro.ing.mobile.banking.android.activity&pli=1">
-                    <div class="badge"></div>
-                </a>
-            </div>
+          <div class="ios">
+            <a
+              target="_blank"
+              href="https://itunes.apple.com/ro/app/ing-homebank/id458609168?mt=8&ls=1"
+            >
+              <div class="badge"></div>
+            </a>
+          </div>
+          <div class="android">
+            <a
+              target="_blank"
+              href="https://play.google.com/store/apps/details?id=ro.ing.mobile.banking.android.activity&pli=1"
+            >
+              <div class="badge"></div>
+            </a>
+          </div>
         </div>
         <div class="help-items">
-            <a class="item" target="_blank" href="https://www.ing.ro/ing-in-romania/contact">
-                Suna-ne
-            </a>
-            <a class="item" target="_blank" href="http://www.youtube.com/user/ingbankromania">
-                Tutoriale
-            </a>
-            <a class="item" target="_blank" href="https://ing.ro/persoane-fizice/internet-banking/homebank#faq">
-                Intrebari frecvente
-            </a>
+          <a
+            class="item"
+            target="_blank"
+            href="https://www.ing.ro/ing-in-romania/contact"
+          >
+            Suna-ne
+          </a>
+          <a
+            class="item"
+            target="_blank"
+            href="http://www.youtube.com/user/ingbankromania"
+          >
+            Tutoriale
+          </a>
+          <a
+            class="item"
+            target="_blank"
+            href="https://ing.ro/persoane-fizice/internet-banking/homebank#faq"
+          >
+            Intrebari frecvente
+          </a>
         </div>
-        <p class="copyright">©2024, ING Bank N.V. Amsterdam, Sucursala București</p>
+        <p class="copyright">
+          ©2024, ING Bank N.V. Amsterdam, Sucursala București
+        </p>
         <a target="_blank" href="http://www.anpc.gov.ro/">
-            <img loading="lazy" width="250" height="50" src="https://www.homebank.ro/hb/hb/assets/images/9978f887a71d4bd370fc.svg" alt="ANPC insolvența persoanelor fizice">
+          <img
+            loading="lazy"
+            width="250"
+            height="50"
+            src="https://www.homebank.ro/hb/hb/assets/images/9978f887a71d4bd370fc.svg"
+            alt="ANPC insolvența persoanelor fizice"
+          />
         </a>
       </footer>
     `;
